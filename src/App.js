1f56e00bac9553871525de4de79196ee39feb54c
mohammaditasmiya2001 <mohammaditasmiya2001@gmail.com>
2022-06-12 14:57:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './comps/Navbar';
+import Stats from './comps/Stats';
+
+function App() {
+  return (
+      <div className=' mr-10 mt-0 pt-0'>
+      <Navbar/>
+      <Stats/>
+    </div>
+  );
+}
+
+export default App;
